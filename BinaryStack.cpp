f1778ccec4b7de2746ad1a0e165f4793b1011049
Nguyen Thien <cristhuuthien@gmail.com>
2023-11-20 10:19:36 +0700

    https://github.com/cristthien/dsa.git

--- BinaryStack.cpp
+++ BinaryStack.cpp
@@ -1,79 +1,83 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    Node* pNext;
-
-
+    Node *pNext;
 };
-Node* initNode(int value) {
-    Node* p = new Node();
+Node *initNode(int value)
+{
+    Node *p = new Node();
     p->data = value;
     p->pNext = NULL;
     return p;
 }
-struct Stack {
-    Node* pTop;
+struct Stack
+{
+    ~Node *pTop;
 };
 
-void initStack(Stack& s) {
+void initStack(Stack &s)
+{
     s.pTop = NULL;
 }
-void push(Stack& s, Node* p) {
-    if (s.pTop == NULL) {
+void push(Stack &s, Node *p)
+{
+    if (s.pTop == NULL)
+    {
         s.pTop = p;
     }
-    else {
+    else
+    {
         p->pNext = s.pTop;
         s.pTop = p;
-
     }
-
 }
 
+bool pop(Stack &s, int &x)
+{
 
-bool pop(Stack& s, int& x) {
-
-    if (s.pTop != NULL) {
-        Node* p = s.pTop;
+    if (s.pTop != NULL)
+    {
+        Node *p = s.pTop;
         s.pTop = s.pTop->pNext;
         x = p->data;
         delete p;
         return true;
     }
-    else { return false; }
-
-
+    else
+    {
+        return false;
+    }
 }
 
-void print(Stack s) {
-    Node* p = s.pTop;
-    while (p != NULL) {
-        cout << p->data ;
+void print(Stack s)
+{
+    Node *p = s.pTop;
+    while (p != NULL)
+    {
+        cout << p->data;
         p = p->pNext;
     }
-
 }
-void ConvertToBinary(Stack &s , int value ){
-    while (value != 0) {
-        push(s, initNode (value%2));
+void ConvertToBinary(Stack &s, int value)
+{
+    while (value != 0)
+    {
+        push(s, initNode(value % 2));
         value = value / 2;
-    
     }
-
 }
 int main()
 {
     Stack s;
     initStack(s);
     int n;
-    cout << "Enter the number you want to convert to binary"<<endl;
+    cout << "Enter the number you want to convert to binary" << endl;
     cin >> n;
     ConvertToBinary(s, n);
     cout << "The result is: ";
     print(s);
     return 0;
-
 }
-
